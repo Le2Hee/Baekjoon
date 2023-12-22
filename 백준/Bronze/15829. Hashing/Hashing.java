80bf53ca75f931943e2cd26b9627ba69dd9db178
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2023-12-22 13:44:19 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14340 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,25 +1,24 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 public class Main {
+	
+	private static final int THRESHOLD = 1600;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long n = Long.parseLong(br.readLine());
-		String[] num = br.readLine().split("");
+		int l = Integer.parseInt(br.readLine());
+		char[] str = br.readLine().toCharArray();
+		BigInteger result = BigInteger.ZERO;
 		
-		HashMap<String, Integer> e = new HashMap<>();
-		for (int i = 1; i <= 26; i++) {
-			e.put(String.valueOf((char) (i + 96)), i);
+		for (int i = 0; i < l; i++) {
+			BigInteger n = BigInteger.valueOf(((int) str[i]) - 96);
+			BigInteger num = BigInteger.valueOf(31).pow(i);
+			
+			result = result.add(n.multiply(num));
 		}
 		
-		n = e.get(num[0]);
-		
-		for (int i = 1; i < num.length; i++) {
-			n += e.get(num[i]) * Math.pow(31, i);
-		}
-		
-		System.out.println(n);
-		
-	}
+		System.out.println(result.remainder(BigInteger.valueOf(1234567891)));
+    }
 }
