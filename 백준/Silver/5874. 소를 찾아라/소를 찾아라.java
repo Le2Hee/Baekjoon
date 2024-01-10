1ef6408faafca1ba1beabeb4f37534a1f950f8f3
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-10 13:54:23 +0900

    [Silver III] Title: 소를 찾아라, Time: 1084 ms, Memory: 19436 KB -BaekjoonHub

---
+++ 백준/Silver/5874. 소를 찾아라/소를 찾아라.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		List<Integer> list = new LinkedList<>();
+		String[] str = br.readLine().split("");
+		int ycount = 0;
+		
+		for (int i = 0; i < str.length - 1; i++) {
+			if (str[i].equals("(") && str[i + 1].equals("(")) {
+				list.add(1); // x
+			}
+			if (str[i].equals(")") && str[i + 1].equals(")")) {
+				list.add(2); // y
+				ycount++;
+			}
+		}
+		
+		int count = 0;
+		
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == 1) {
+				count += ycount;
+			} else {
+				ycount--;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
