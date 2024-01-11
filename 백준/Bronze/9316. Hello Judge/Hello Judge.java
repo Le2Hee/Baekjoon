30d3af5b789b81813941835c72f96350aa90bcf9
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-11 11:34:47 +0900

    [Bronze IV] Title: Hello Judge, Time: 120 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= n; i++) {
+			sb.append("Hello World, Judge ").append(i).append("!").append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
