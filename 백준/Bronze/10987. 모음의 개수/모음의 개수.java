c2858ccd0bd5feb6ee0e2603abcafd6d6605810a
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-15 15:15:40 +0900

    [Bronze III] Title: 모음의 개수, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String str = br.readLine();
+		int count = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+				count++;
+			}
+		}
+		
+		sb.append(count);
+		System.out.println(sb);
+	}
+}
