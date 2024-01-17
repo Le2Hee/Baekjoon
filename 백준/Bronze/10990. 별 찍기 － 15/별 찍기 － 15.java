8364f47d2b05444e7bca0f84784a5a6ff0d46a0e
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-17 17:51:28 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 144 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				sb.append(" ");
+			}
+			
+			for (int j = 1; j <= i * 2 - 1; j++) {
+				if (j == 1 || j == i * 2 - 1) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+    }
+}
