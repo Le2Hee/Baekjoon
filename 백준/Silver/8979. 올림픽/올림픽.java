7d40d200a33ce487eeff38b662135ccdfbf27835
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-18 15:49:53 +0900

    [Silver V] Title: 올림픽, Time: 172 ms, Memory: 14832 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -3,42 +3,89 @@ import java.util.*;
 
 public class Main {
 	
+	public static class Country {
+		int name, gold, silver, bronze, rank;
+		
+		public Country(int n, int g, int s, int b, int r) {
+			name = n;
+			gold = g;
+			silver = s;
+			bronze = b;
+			rank = r;
+		}
+
+		public void setRank(int rank) {
+			this.rank = rank;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		List<int[]> list = new LinkedList<>();
-		int count = 0;
+		int m = Integer.parseInt(st.nextToken());
+		Country ct;
+		List<Country> list = new LinkedList<>();
 		
-		for (int i = 0; i < n; i++) {
+		while (n-- > 0) {
 			st = new StringTokenizer(br.readLine());
-			
-			int num = Integer.parseInt(st.nextToken());
+			int name = Integer.parseInt(st.nextToken());
 			int g = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			int[] nums = {num, g, s, b};
-			list.add(nums);
+			ct = new Country(name, g, s, b, 1);
+			list.add(ct);
 		}
 		
-		list.sort(Comparator.comparingInt((int[] a) -> a[1]).reversed().thenComparingInt(a -> a[3]).reversed().thenComparingInt(a -> a[2]).reversed());
-		int index = 0;
+		Collections.sort(list, new Comparator<Country>() {
+			@Override
+			public int compare(Country o1, Country o2) {
+				int num = 0;
+				
+				if (o1.gold - o2.gold < 0) {
+					num = 1;
+				} else if (o1.gold - o2.gold == 0) {
+					if (o1.silver - o2.silver < 0) {
+						num = 1;
+					} else if (o1.silver - o2.silver == 0) {
+						if (o1.bronze - o2.bronze < 0) {
+							num = 1;
+						} else if (o1.bronze - o2.bronze == 0) {
+							num = 0;
+						} else {
+							num = -1;
+						}
+					} else {
+						num = -1;
+					}
+				} else {
+					num = -1;
+				}
+				
+				return num;
+			}
+		});
 		
-		for (int i = 0; i < n; i++) {
-			if (list.get(i)[0] == k) {
-				index = i;
+		int result = 0;
+		
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i - 1).gold == list.get(i).gold && list.get(i - 1).silver == list.get(i).silver && list.get(i - 1).bronze == list.get(i).bronze) {
+				list.get(i).setRank(list.get(i - 1).rank);
+			} else {
+				list.get(i).setRank(i + 1);
+			}
+			
+			if (list.get(i).name == m) {
+				result = list.get(i).rank;
 			}
 		}
 		
-		for (int i = index - 1; i >= 0; i--) {
-			if (list.get(index)[1] == list.get(i)[1] && list.get(index)[2] == list.get(i)[2] && list.get(index)[3] == list.get(i)[3]) {
-				count++;
-			}
+		if (result == 0) {
+			result = list.get(0).rank;
 		}
 		
-		System.out.println(index + 1 - count);
+		System.out.println(result);
 	}
 }
