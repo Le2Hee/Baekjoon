63f64beb2ac421da395c47e045447a3636615c25
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-18 17:56:00 +0900

    [Bronze V] Title: Can you add this?, Time: 116 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		while (n-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			long sum = a + b;
+			sb.append(sum).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
