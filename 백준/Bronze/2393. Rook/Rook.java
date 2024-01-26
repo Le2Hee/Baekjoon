74ecacde00ee80ff650930fc20e955d9a0972b4d
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-26 11:40:01 +0900

    [Bronze V] Title: Rook, Time: 124 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\");
+		System.out.println(sb);
+	}
+}
