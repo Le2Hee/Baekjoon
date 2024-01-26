c80332074d93326b01167a46591b9d3642ee55d6
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-01-26 10:51:48 +0900

    [Bronze III] Title: 세 수, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		if (a + b == c) {
+			sb.append(a).append("+").append(b).append("=").append(c);
+		} else if (a == b + c) {
+			sb.append(a).append("=").append(b).append("+").append(c);
+		} else if (a - b == c) {
+			sb.append(a).append("-").append(b).append("=").append(c);
+		} else if (a == b - c) {
+			sb.append(a).append("=").append(b).append("-").append(c);
+		} else if (a * b == c) {
+			sb.append(a).append("*").append(b).append("=").append(c);
+		} else if (a == b * c) {
+			sb.append(a).append("=").append(b).append("*").append(c);
+		} else if (a / b == c) {
+			sb.append(a).append("/").append(b).append("=").append(c);
+		} else if (a == b / c) {
+			sb.append(a).append("=").append(b).append("/").append(c);
+		}
+		
+		System.out.println(sb);
+	}
+}
