79b6c2f961afa81adcf998c3ecc1ebf6f6b442b4
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-02-01 15:24:09 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static boolean[] visit;
+	static int[] arr, numbers;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		numbers = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numbers);
+		arr = new int[m];
+		visit = new boolean[n];
+		back(0, 0);
+		System.out.println(sb);
+	}
+	
+	private static void back(int start, int depth) {
+		if (m == depth) {
+			for (int num : arr) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			visit[i] = true;
+			arr[depth] = numbers[i];
+			back(i + 1, depth + 1);
+			visit[i]  = false;
+		}
+	}
+}
