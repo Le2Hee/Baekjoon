ca977014296e04d76bd28f8954bfad7a82822688
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-02-07 17:34:36 +0900

    [Bronze V] Title: Serca, Time: 320 ms, Memory: 54192 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		
+		while (t-- > 0) {
+			sb.append(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n");
+		}
+		
+		System.out.println(sb);
+	}
+}
