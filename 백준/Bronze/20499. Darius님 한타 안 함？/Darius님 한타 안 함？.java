680ac6bf525f2393bdfa1f2a39dedd09feaa7bb7
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-03-21 15:29:14 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), "/");
+		int k = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
+		
+		if (k + a < d || d == 0) {
+			sb.append("hasu");
+		} else {
+			sb.append("gosu");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
