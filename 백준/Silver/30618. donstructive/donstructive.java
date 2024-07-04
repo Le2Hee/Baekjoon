fede0de5d9ae4b930178169030418fb0a725b52b
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-07-04 14:49:46 +0900

    [Silver IV] Title: donstructive, Time: 308 ms, Memory: 31356 KB -BaekjoonHub

---
+++ 백준/Silver/30618. donstructive/donstructive.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine()); // 순열의 길이
+		
+		Deque<Integer> qu = new LinkedList<>();
+		boolean check = false;
+		
+		for (int i = n; i >= 1; i--) {
+			if (!check) {
+				qu.addLast(i);
+			} else {
+				qu.addFirst(i);
+			}
+			
+			check = !check;
+		}
+		
+		for (int num : qu) {
+			sb.append(num).append(" ");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
