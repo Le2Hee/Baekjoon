77eb52a6c71d81a6555b610732b0666df67bda9e
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-07-25 15:59:20 +0900

    [Bronze III] Title: 보물 찾기, Time: 248 ms, Memory: 20616 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.java
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.java
@@ -9,32 +9,22 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
 		
-		int l, r, s, num;
+		int l, r, s, ls, rs;
 		StringTokenizer st;
 		while (t-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			l = Integer.parseInt(st.nextToken()); // 왼쪽 보물
 			r = Integer.parseInt(st.nextToken()); // 오른쪽 보물
 			s = Integer.parseInt(st.nextToken()); // 현재 위치
-			num = 1; // 이동 거리
 			
-			for (int i = 0; ; i++) {
-				if (i % 2 == 0) { // 짝수이면 오른쪽 이동
-					s += num;
-				} else { // 홀수이면 왼쪽 이동
-					s -= num;
-				}
-				
-				// 보물을 찾았다면
-				if (s == l || s == r) {
-					break;
-				} else {
-					num++;
-				}
+			ls = s - l;
+			rs = r - s;
+			
+			if (ls < rs) {
+				sb.append(ls * 2 + 1).append("\n");
+			} else {
+				sb.append(2 * rs).append("\n");
 			}
-			
-			// 첫 위치에서 조사를 하기 때문에 +1
-			sb.append(num + 1).append("\n");
 		}
 		
 		bw.write(sb.toString());
