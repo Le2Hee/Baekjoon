c09748f4108b47b8d0bcd560077e7e64e2df94b7
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-07-26 14:42:37 +0900

    [Bronze II] Title: 숫자의 개수 2, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/21567. 숫자의 개수 2/숫자의 개수 2.java
+++ 백준/Bronze/21567. 숫자의 개수 2/숫자의 개수 2.java
@@ -3,41 +3,14 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-	private static final int BUF_SIZE = 1 << 13;
-    private static final byte[] buf = new byte[BUF_SIZE];
-    private static int cursor = -1, end;
-    
-    private static byte read() throws IOException {
-        if (++cursor == end) {
-            end = System.in.read(buf, 0, BUF_SIZE);
-            cursor = 0;
-        }
-        
-        return buf[cursor];
-    }
-    
-    private static int readInt() throws IOException {
-        int res = 0;
-        int in = read();
-        // 공백 및 줄 바꿈 문자 무시
-        while (in <= 32) {
-            in = read();
-        }
-        // 숫자 읽기
-        while (in > 32) {
-            res = (res << 3) + (res << 1) + (in & 15);
-            in = read();
-        }
-        return res;
-    }
     
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		int a = readInt();
-		int b = readInt();
-		int c = readInt();
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
 		
 		BigInteger mul = BigInteger.valueOf(a).multiply(BigInteger.valueOf(b)).multiply(BigInteger.valueOf(c)); 
 		
