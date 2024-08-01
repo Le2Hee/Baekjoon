d22006407dddc6cf6f1b48de7d0ed54f1a5c8950
Le2Hee <127175426+Le2Hee@users.noreply.github.com>
2024-08-01 14:27:05 +0900

    [Bronze I] Title: 비숍 투어, Time: 128 ms, Memory: 14304 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/23885. 비숍 투어/비숍 투어.java
+++ 백준/Bronze/23885. 비숍 투어/비숍 투어.java
@@ -8,20 +8,26 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		// 체스판의 가로 세로는 사용하지 않아도 된다.
+		int n = Integer.parseInt(st.nextToken()); // 체스판의 가로
+		int m = Integer.parseInt(st.nextToken()); // 체스판의 세로
 		
 		// 위치는 짝수라면 true 홀수라면 false로 설정
 		st = new StringTokenizer(br.readLine());
-		boolean startx = Integer.parseInt(st.nextToken()) % 2 == 0 ? true : false;
-		boolean starty = Integer.parseInt(st.nextToken()) % 2 == 0 ? true : false;
+		int startx = Integer.parseInt(st.nextToken());
+		int starty = Integer.parseInt(st.nextToken());
+		boolean sbx = startx % 2 == 0 ? true : false;
+		boolean sby = starty % 2 == 0 ? true : false;
 		
 		st = new StringTokenizer(br.readLine());
-		boolean endx = Integer.parseInt(st.nextToken()) % 2 == 0 ? true : false;
-		boolean endy = Integer.parseInt(st.nextToken()) % 2 == 0 ? true : false;
+		int endx = Integer.parseInt(st.nextToken());
+		int endy = Integer.parseInt(st.nextToken());
+		boolean ebx = endx % 2 == 0 ? true : false;
+		boolean eby = endy % 2 == 0 ? true : false;
 		
-		
-		// 갈 수 있는 경우만 체크
-		// 시작위치의 y, x좌표 둘 다 짝수일 때
+		// 체스판의 크기가 둘 중에 하나가 1이라면
+		// 도착과 시작의 위치가 같아야한다.
+		//
+		// 시작위치의 y좌표 짝수, x좌표 짝수
 		// 1. 도착위치 y좌표 짝수, x좌표 짝수
 		// 2. 도착위치 y좌표 홀수, x좌표 홀수
 		//
@@ -36,14 +42,18 @@ public class Main {
 		// 시작위치 y좌표 홀수, x좌표 홀수
 		// 1. 도착위치 y좌표 짝수, x좌표 짝수
 		// 2. 도착위치 y좌표 홀수, x좌표 홀수
-		if (startx == endx && starty == endy) {
-			sb.append("YES");
-		} else if (startx != endx && starty != endy) {
-			sb.append("YES");
-		}
 		
-		// 갈 수 없다면
-		if (sb.length() == 0) {
+		if ((n == 1 || m == 1)) {
+			if (startx == endx && starty == endy) {
+				sb.append("YES");
+			} else {
+				sb.append("NO");
+			}
+		} else if (sbx != sby && ebx != eby) {
+			sb.append("YES");
+		} else if (sbx == sby && ebx == eby) {
+			sb.append("YES");
+		} else {
 			sb.append("NO");
 		}
 		
